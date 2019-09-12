466fc67962da7aa9ac26629582b8d56fac14b902
Mairah <animefreaknor@hotmail.com>
2019-09-12 15:39:29 +1000

    Edited list component ts

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProddataService } from '../services/proddata.service';
+import { Product } from '../product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  products: Product[] = [];
+  constructor(private proddata: ProddataService, private router: Router) { }
 
   ngOnInit() {
+    this.proddata.read().subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  deleteProd(id){
+    if (confirm("Are you sure you want to delete this item?")){
+      this.proddata.delete(id).subscribe((data) => {
+        this.products = data;
+      });
+    }
   }
 
 }
